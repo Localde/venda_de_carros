eee2078e5da0b75c222dce39b588d3a7609d8953
Ollywer <dyegoollywer@gmail.com>
2020-12-09 16:36:49 -0300

    09-12-20

--- gerenciamento.php
+++ gerenciamento.php
@@ -4,6 +4,42 @@
 	<title>Itaipú Veiculos</title>
 	<meta charset="utf-8"/>
 	<link rel="stylesheet" type="text/css" href="estilos.css"/>
+	<script src="jquery-3.5.1.min.js"></script>
+	<script>
+		$(document).ready(function(){
+			$("#menub1, #menub2, #menub3, #menub4").css("visibility", "hidden");
+			$("#menua1").click(function(){
+				$("#menub1").css("visibility", "visible");
+				$("#menub2").css("visibility", "hidden");
+				$("#menub3").css("visibility", "hidden");
+				$("#menub4").css("visibility", "hidden");
+			});
+			$("#menua2").click(function(){
+				$("#menub2").css("visibility", "visible");
+				$("#menub1").css("visibility", "hidden");
+				$("#menub3").css("visibility", "hidden");
+				$("#menub4").css("visibility", "hidden");
+			});
+			$("#menua3").click(function(){
+				$("#menub3").css("visibility", "visible");
+				$("#menub1").css("visibility", "hidden");
+				$("#menub2").css("visibility", "hidden");
+				$("#menub4").css("visibility", "hidden");
+			});
+			$("#menua4").click(function(){
+				$("#menub4").css("visibility", "visible");
+				$("#menub3").css("visibility", "hidden");
+				$("#menub2").css("visibility", "hidden");
+				$("#menub1").css("visibility", "hidden");
+			});
+			$("#menub1","#menub2","#menub3", "#menu4").mouseover(function(){
+					$(this).css("visibility","visible");
+			});
+			$("#menub1","#menub2","#menub3", "#menu4").mouseout(function(){
+					$(this).css("visibility","hidden");
+			});
+		});
+	</script>
 </head>
 <body>
 
@@ -20,7 +56,7 @@
 	<nav>
 		<div class="menu_ger">
 			<button id="menua1" class="btmenu">carros</button>
-			<div id="munub1" class="menub">
+			<div id="menub1" class="menub">
 				<a href="#" target="_self">novo</a>
 				<a href="#" target="_self">editar</a>
 				<a href="#" target="_self">excluir</a>
